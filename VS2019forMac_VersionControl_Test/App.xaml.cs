844b1d72b4235bdf76e39e73ba04a490bab414cc
LeopoldoKnett <Leopoldo.Knett@gmail.com>
2020-03-27 08:31:00 +0000

    2020.03.28-a

---
+++ VS2019forMac_VersionControl_Test/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace VS2019forMac_VersionControl_Test
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
